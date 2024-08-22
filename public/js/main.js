7ec7e586e4a2fc608b60de4c609585891af24e40
Natali Schwartz <schwartznatali@gmail.com>
2024-08-22 01:08:26 -0300

    add media queries for all devices

--- public/js/main.js
+++ public/js/main.js
@@ -31,7 +31,48 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
 
-/*=============== CALCULATE JS ===============*/
-
-
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message'),
+    contactUser = document.getElementById('contact-user')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    // Check if the field has a value
+    if(contactUser.value ===""){
+        // Add and remove color
+        contactMessage.classList.remove('color-green')
+        contactMessage.classList.add('color-red')
+
+        // Show message
+        contactMessage.textContent = 'You must enter your email 👆🏻'
+
+        // Remove message three seconds
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 3000)
+    }else{
+        // serviceID - templateID - #form - publicKey
+        emailjs.sendForm('service_1pw0tun', 'template_4icxwb4','#contact-form', 'XPE5hIxIYXXGy8r0O')
+            .then(() =>{
+                // Show message and add color
+                contactMessage.classList.add('color-green')
+                contactMessage.textContent = "You registered successfully"
+
+                // Remove message after three seconds
+                setTimeout(() =>{
+                    contactMessage.textContent = ''
+                }, 3000)
+            }, (error)=>{
+                // Mail sending error
+                alert('OOPS! SOMETHING HAS FAILED...',error)
+            })
+
+        // To clear the input field
+        contactUser.value = '';
+    }
+
+}
+
+contactForm.addEventListener('submit' , sendEmail)
