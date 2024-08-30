37c85d66c07bc7850072bc90f37093780346b9fa
Natali Schwartz <schwartznatali@gmail.com>
2024-08-30 15:30:23 -0300

    change logos styles and add a blog section

--- public/js/main.js
+++ public/js/main.js
@@ -21,7 +21,16 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
-
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // when the scroll is higher than 560 viewport height add the show-scroll class
+    if (this.scrollY >= 560){
+        scrollUp.classList.add('show-scroll');
+    }else{
+        scrollUp.classList.remove('show-scroll')
+    } 
+}
+window.addEventListener('scroll', scrollUp);
 
 
 
