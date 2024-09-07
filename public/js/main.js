2f72581880a1d8c9f9d46f9287c0a7cc62958f5d
Natali Schwartz <schwartznatali@gmail.com>
2024-09-07 15:46:04 -0300

    add a new section Our League

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,15 @@
 const navClose = document.querySelector('#nav-close');
 const navMenu = document.querySelector('#navbarNavDropdown');
 
+
 /* Menu hidden */
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show')
     })
 }
+
+
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
@@ -65,7 +68,7 @@ const sendEmail =(e) =>{
                 }, 3000)
             }else{
                  // serviceID - templateID - #form - publicKey
-                emailjs.sendForm('service_1pw0tun', 'template_4icxwb4','#contact-form', 'XPE5hIxIYXXGy8r0O',templateParams)
+                emailjs.sendForm('service_kus0m4k', 'template_leb5d6p','#contact-form', 'mJdOHv7r9Tbe2wMYO',templateParams)
                 .then(function(response) {
                     console.log('SUCCESS!', response.status, response.text);
                     alert('Mensaje enviado exitosamente!');
@@ -79,51 +82,3 @@ const sendEmail =(e) =>{
     contactForm.addEventListener('submit' , sendEmail)
 
 
-
-
-// const contactForm = document.getElementById('contact-form'),
-//     contactMessage = document.getElementById('contact-message'),
-//     contactUser = document.getElementById('contact-user')
-//     contactTextarea = document.getElementById('contact-message')
-
-// const sendEmail = (e) =>{
-//     e.preventDefault()
-
-//     // Check if the field has a value
-//     if(contactUser.value ===""){
-//         // Add and remove color
-//         contactMessage.classList.remove('color-green')
-//         contactMessage.classList.add('color-red')
-
-//         // Show message
-//         contactMessage.textContent = 'You must enter your email 👆🏻'
-
-//         // Remove message three seconds
-//         setTimeout(() =>{
-//             contactMessage.textContent = ''
-//         }, 3000)
-//     }else{
-//         // serviceID - templateID - #form - publicKey
-//         emailjs.sendForm('service_1pw0tun', 'template_4icxwb4','#contact-form', 'XPE5hIxIYXXGy8r0O')
-//             .then(() =>{
-//                 // Show message and add color
-//                 contactMessage.classList.add('color-green')
-//                 contactMessage.textContent = "You registered successfully"
-
-
-//                 // Remove message after three seconds
-//                 setTimeout(() =>{
-//                     contactMessage.textContent = ''
-//                 }, 3000)
-//             }, (error)=>{
-//                 // Mail sending error
-//                 alert('OOPS! SOMETHING HAS FAILED...',error)
-//             })
-
-//         // To clear the input field
-//         contactUser.value = '';
-//     }
-
-// }
-
-// contactForm.addEventListener('submit' , sendEmail)
